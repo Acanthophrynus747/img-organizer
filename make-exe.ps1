8d6f3d56aa272b693ecc55467687260e3965df72
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-07-29 01:06:18 -0700

    renamed files

--- make-exe.ps1
+++ make-exe.ps1
@@ -1,5 +1,5 @@
-pyinstaller --name img_mover --icon=.\assets\img-org-icon.ico --onefile file_organizer.py; #command line version
+pyinstaller --name img-mover --icon=.\assets\img-org-icon.ico --onefile img-organizer.py; #command line version
 
-pyinstaller --name img-move-gui --icon=.\assets\img-org-icon.ico --onefile file-org-gui.py; #gui version
+pyinstaller --name img-mover-gui --icon=.\assets\img-org-icon.ico --onefile file-org-gui.py; #gui version
 
-Read-Host -Prompt "Done. Press Enter to exit";
+Read-Host -Prompt "Done! Press Enter to exit";

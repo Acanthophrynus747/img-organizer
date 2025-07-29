9b9cf1bb61e6d21de41688460d48949dfd127ac8
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-07-29 00:49:56 -0700

    improved UI on terminal version

--- file_organizer.py
+++ file_organizer.py
@@ -19,54 +19,69 @@ import tkinter as tk
 from tkinter import filedialog
 import shutil
 
-root = tk.Tk()
-root.withdraw()
+try:
 
-print("First select the folder with the images you want to move and organize, then select the folder to move them into.")
-print("The photos will be organized by date.")
+    root = tk.Tk()
+    root.withdraw()
 
-source_dir_path = filedialog.askdirectory()
-print(source_dir_path)
+    print("First select the folder with the images you want to move and organize, then select the folder to move them into.")
+    print("")
+    print("The photos will be organized by date.")
+    print("----------------------------------------------------------------------------------------------------------------")
+    input("Press Enter to select the desired source directory:")
 
-destination_dir_path = filedialog.askdirectory()
-print(destination_dir_path)
+    source_dir_path = filedialog.askdirectory()
 
-source_dir_contents = os.listdir(source_dir_path)
+    source_dir_contents = os.listdir(source_dir_path)
+    num_images = len(source_dir_contents)
 
-num_images = len(source_dir_contents)
+    print(f"Moving {num_images} images from {source_dir_path}")
+    print("")
 
-# print(source_dir_contents)
+    input("Press Enter to select the desired destination directory:")
 
-for i in range(len(source_dir_contents)):
-    image_file_name = source_dir_contents[i]
-    image_path = os.path.join(source_dir_path, image_file_name)
-    # print(image_path)
+    destination_dir_path = filedialog.askdirectory()
 
-    image = Image.open(image_path)
-    image.load()
-    # print(image)
+    print(f"Moving organized images to {destination_dir_path} in folders by date")
+    print("")
 
-    exifdata = image.getexif()
+    input("Press Enter to proceed:")
 
-    date_time = exifdata.get(306)
-    split_date_time = date_time.split()
-    date = split_date_time[0]
-    date_reformatted = date.replace(":", "-")
+    # print(source_dir_contents)
 
-    destination_dir_contents = os.listdir(destination_dir_path)
+    for i in range(len(source_dir_contents)):
+        image_file_name = source_dir_contents[i]
+        image_path = os.path.join(source_dir_path, image_file_name)
+        # print(image_path)
 
-    if date_reformatted not in destination_dir_contents:
-        new_folder_path = os.path.join(destination_dir_path, date_reformatted)
-        print(f"Creating directory {date_reformatted} in {destination_dir_path}")
-        os.mkdir(new_folder_path)
-        shutil.copy2(image_path, new_folder_path)
-    else:
-        shutil.copy2(image_path, os.path.join(destination_dir_path, date_reformatted))
+        image = Image.open(image_path)
+        image.load()
+        # print(image)
 
-    print(f"Moved {image_file_name} to directory {date_reformatted}. ({i}/{num_images})")
+        exifdata = image.getexif()
 
-# for tag_id in exifdata:
-#     tag_name = TAGS.get(tag_id, tag_id)
-#     value = exifdata.get(tag_id)
-#     print(f"{tag_id}: {tag_name}: {value}")
-# print(img.info)
+        date_time = exifdata.get(306)
+        split_date_time = date_time.split()
+        date = split_date_time[0]
+        date_reformatted = date.replace(":", "-")
+
+        destination_dir_contents = os.listdir(destination_dir_path)
+
+        if date_reformatted not in destination_dir_contents:
+            new_folder_path = os.path.join(destination_dir_path, date_reformatted)
+            print(f"Creating directory {date_reformatted} in {destination_dir_path}")
+            os.mkdir(new_folder_path)
+            shutil.copy2(image_path, new_folder_path)
+        else:
+            shutil.copy2(image_path, os.path.join(destination_dir_path, date_reformatted))
+
+        print(f"Moved {image_file_name} to directory {date_reformatted}. ({i}/{num_images})")
+
+    # for tag_id in exifdata:
+    #     tag_name = TAGS.get(tag_id, tag_id)
+    #     value = exifdata.get(tag_id)
+    #     print(f"{tag_id}: {tag_name}: {value}")
+    # print(img.info)
+
+except KeyboardInterrupt:
+    exit()

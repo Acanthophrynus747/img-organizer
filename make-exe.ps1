7b940712c89594fb08f57bffc284009e8e667967
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-07-29 00:52:55 -0700

    put icon stuff in its own folder for organization

--- make-exe.ps1
+++ make-exe.ps1
@@ -1,5 +1,5 @@
-pyinstaller --name img_mover --icon=img-org-icon.ico --onefile file_organizer.py; #command line version
+pyinstaller --name img_mover --icon=.\assets\img-org-icon.ico --onefile file_organizer.py; #command line version
 
-pyinstaller --name img-move-gui --icon=img-org-icon.ico --onefile file-org-gui.py; #gui version
+pyinstaller --name img-move-gui --icon=.\assets\img-org-icon.ico --onefile file-org-gui.py; #gui version
 
 Read-Host -Prompt "Done. Press Enter to exit";
